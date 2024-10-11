bae4720219667c23bef50e820357f644c43741fd
yadsashel <ysalhi600@gmail.com>
2024-10-11 17:03:28 +0100

    task 0 is done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-"""
-a type-annotated function that takes a float a and a
-float b as arguments and returns their sum as a float
-"""
+'''Modules of task-0.
+'''
 
 
 def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
     return a + b
