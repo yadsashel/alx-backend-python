191c7ab5f9a4d7b2566bff2406dc871ecac422ef
yadsashel <ysalhi600@gmail.com>
2024-10-11 17:34:58 +0100

    checking for the last advanced tasks

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""This function safely retrieves a value from a dictionary
-given a key, returning a default value if the key is not found."""
+""" More involved type annotations  """
+from typing import Mapping, Any, Sequence, Union, TypeVar
 
-from typing import Mapping, Any, Optional, TypeVar
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any,
-                     default: Optional[T] = None) -> Optional[T]:
-    """Return the value associated with key in the dictionary dct
-    or default if the key is not found."""
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """ Safely get value """
     if key in dct:
         return dct[key]
     else:
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-"""This function returns a zoomed-in version of a list,
-duplicating each element a specified number of times."""
-
-from typing import List
+""" Type Checking  """
+from typing import Tuple, List
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
-    """Return a list with each element of lst duplicated factor times."""
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Zoom Array """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
